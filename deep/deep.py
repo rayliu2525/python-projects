5afaca6067488feae79bf923fd0564460a07bf76
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-19 19:24:04 +0000

    Tue, Apr 19, 2022, 7:24 PM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,10 +1,10 @@
 x = input("What is the answer to the Great Question of Life, the Universe and Everything?: ").lower()
 
-if x == "42"
+if x == "42":
     print("Yes")
-elif x == "forty-two"
+elif x == "forty-two":
     print("Yes")
-elif x == "forty two"
+elif x == "forty two":
     print("Yes")
 else:
     print("No")
