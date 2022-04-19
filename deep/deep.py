25dba5785603465161887fcb2b17d5f7f45fc13b
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-19 19:20:58 +0000

    Tue, Apr 19, 2022, 7:20 PM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,6 @@
-x = input("What is the answer to the Great Question of Life, the Universe and Everything?: ")
+x = input("What is the answer to the Great Question of Life, the Universe and Everything?: ").lower()
 
 if x == "42"
     print(x)
-elif x ==
+elif x == "forty-two"
+    
