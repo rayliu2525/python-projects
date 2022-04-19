bd7878ed96e0cc92decd7565b03c97bbb6fadb70
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-19 21:24:44 +0000

    Tue, Apr 19, 2022, 9:24 PM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,8 +1,8 @@
 x = input("Type your greeting: ").lower().strip()
 
-if x[0:4] == "hello"
+if x[0:4] == "hello":
     print("$0")
-elif x[0] == "h"
+elif x[0] == "h":
     print("$20")
 else:
     print("$100")
