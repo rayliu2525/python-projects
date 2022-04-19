a5fd8f09e6efe46282a9d753229938fb1993e986
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-19 03:16:12 +0000

    Tue, Apr 19, 2022, 3:16 AM +00:00

--- faces/faces.py
+++ faces/faces.py
@@ -2,7 +2,9 @@ def convert(param1):
     x = param1.replace(":)", "🙂").replace(":(", "🙁")
     return x
 
+def main():
+    y = input("write your sentence: ")
+    z = convert(y)
+    print(z)
 
-y = input("write your sentence: ")
-z = convert(y)
-print(z)
+main()
