54fee841474279e9148321fde2c46ddfb17a972e
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-20 22:16:25 +0000

    Wed, Apr 20, 2022, 10:16 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,8 +1,11 @@
 x = input("Name of variable in camel case: ")
 l = []
+
 for letter in x:
-    if isupper(letter) = True:
-        l.append(f"_{letter.lower()})
+    if isupper(letter) == True:
+        l.append(f"_{letter.lower()}")
     else:
         l.append(letter)
-"".join(l)
+
+new_list = "".join(l)
+print(new_list)
