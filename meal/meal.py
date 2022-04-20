c093949ba57b06fe6b13a67098b2d9a7606f26f6
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-20 04:35:31 +0000

    Wed, Apr 20, 2022, 4:35 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,7 +1,12 @@
 def main():
     x = input("What time is it?: ")
     conv_time = convert(x)
-    if 8 =<
+    if 7 <= conv_time <= 8:
+        print("breakfast time")
+    elif 12 <= conv_time <= 13:
+        print("lunch time")
+    elif 18 <= conv_time <= 19:
+        print("dinner time")
 
 def convert(time):
     x, y = time.split(":")
