f9dc34431e1213a87d1138f031057ed77b117d0e
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-20 02:12:41 +0000

    Wed, Apr 20, 2022, 2:12 AM +00:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,10 +1,10 @@
 answer = input("expression: ")
 x, y, z = answer.split(" ")
 if y == "+":
-    print(int(x) + int(y))
+    print(int(x) + int(z))
 elif y == "-":
-    print(int(x) - int(y))
+    print(int(x) - int(z))
 elif y == "*":
-    print(int(x) * int(y))
+    print(int(x) * int(z))
 elif y == "/":
-    print(int(x) / int(y))
+    print(int(x) / int(z))
