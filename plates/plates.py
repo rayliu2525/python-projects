4fbd523719af4abd64933060711909b53e774737
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-21 20:49:17 +0000

    Thu, Apr 21, 2022, 8:49 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -21,7 +21,7 @@ def is_valid(s):
 
 def num_at_end(s):
     for char in s:
-        if char in list(range(0,10)):
+        if char in list(range(1,10)):
             if string.ascii_uppercase not in s[s.index[char]:]:
                 return True
             else:
