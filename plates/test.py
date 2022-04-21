164ba3349e35749f0f46c2d0bd59890357b07358
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-21 21:12:00 +0000

    Thu, Apr 21, 2022, 9:12 PM +00:00

--- plates/test.py
+++
@@ -1,6 +0,0 @@
-s = "5"
-
-for char in s:
-    if char not in [str(char1) for char1 in range(0,10)]:
-        print("hello")
-pass
--- plates/test2.py
+++
@@ -1,2 +0,0 @@
-if "11" not in [str(char1) for char1 in range(0,10)]:
-    print("yes")
