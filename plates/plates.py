415beff4ae18ae052fe623cb57eef6927a05b180
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-21 15:30:54 +0000

    Thu, Apr 21, 2022, 3:30 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,11 +9,14 @@ def main():
 def is_valid(s):
     if s[0] and s[1] in ascii_uppercase:
         if 2 <= len(s) <= 6:
-            if s[2:] in range(0,10)
+            for char in s:
+
 
             else:
                 # make sure all are uppercase if no numbers
                 return True
+
+def
 
 
 main()
