aca4b62162a4f44e6b97cba72ba292e4c958748e
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-21 20:35:29 +0000

    Thu, Apr 21, 2022, 8:35 PM +00:00

--- plates/test.py
+++ plates/test.py
@@ -1,5 +1,6 @@
 s = "5"
 
-if charr not in [str(char1) for char1 in range(0,10)]:
-    print("hello")
+for char in s:
+    if charr not in [str(char1) for char1 in range(0,10)]:
+        print("hello")
 pass
