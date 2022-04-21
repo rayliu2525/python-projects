aab7a5c017166ede79629fa2f78a3c974976a722
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-21 20:34:28 +0000

    Thu, Apr 21, 2022, 8:34 PM +00:00

--- plates/test.py
+++ plates/test.py
@@ -1,5 +1,5 @@
-s = "CS50"
+s = "5"
 
-for charr in s:
-    if charr not in [str(char1) for char1 in range(0,10)]:
-        print("hello")
+if charr not in [str(char1) for char1 in range(0,10)]:
+    print("hello")
+pass
