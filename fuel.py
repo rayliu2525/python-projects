f10d7a7ed95ff43329c00c4589712e019d839fd6
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-27 14:48:31 +0000

    Wed, Apr 27, 2022, 2:48 PM +00:00

--- fuel.py
+++ fuel.py
@@ -7,13 +7,14 @@ while True:
 
     x = int(x)
     y = int(y)
-    gas_percent = f"{round(x / y * 100)}%"
 
     try:
         x / y
     except ZeroDivisionError:
         print("try again")
         continue
+
+    gas_percent = f"{round(x / y * 100)}%"
 
     if x > y:
         continue
