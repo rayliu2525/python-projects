fa8c3e705f83b711bf4ad28c2cc3a08d00baa442
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-04-27 13:36:42 +0000

    Wed, Apr 27, 2022, 1:36 PM +00:00

--- fuel.py
+++ fuel.py
@@ -1,18 +1,20 @@
+while True:
+
+    x, y = input("enter an amount: " ).split(/)
+    gas_percent = round(x / y * 100)
+
+    if x / y * 100 < 1:
+        print("E)
+        break
+
+    elif x > y:
 
 
-x, y = input("enter an amount: " ).split(/)
-gas_percent = round(x / y * 100)
+    elif x / y * 100 > 99:
+        print("F")
+        break
 
-if x / y * 100 < 1:
-    print("E)
-
-elif x > y:
-
-
-elif x / y * 100 > 99:
-    print("F")
-
-else:
-    print(gas_percent)
-
+    else:
+        print(gas_percent)
+        break
 
