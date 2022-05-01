291a9fa68c5ac960c365da05ed67fdc7a8a0acde
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-01 20:51:31 +0000

    Sun, May 1, 2022, 8:51 PM +00:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,20 +1,22 @@
-item = title(lower(input("Enter Item: ")))
+while True:
 
-menu = {
-    "Baja Taco": 4.00,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
-}
+    item = title(lower(input("Enter Item: ")))
 
-total = 0
+    menu = {
+        "Baja Taco": 4.00,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
 
-if item in menu:
-    total += menu[item]
-    print f"${total}"
+    total = 0
+
+    if item in menu:
+        total += menu[item]
+        print(f"${total}")
 
