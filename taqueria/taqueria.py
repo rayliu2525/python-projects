bf545ef33cef474bb56319bf73c9693f2d2fdd86
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-01 20:28:30 +0000

    Sun, May 1, 2022, 8:28 PM +00:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,1 +1,13 @@
-item = lower(input("Enter Item: "))
+item = title(lower(input("Enter Item: ")))
+
+if item in {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
