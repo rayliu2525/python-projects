69886331ee877a477ae674e952707c4a6d439ce0
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-04 20:14:44 +0000

    Wed, May 4, 2022, 8:14 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -6,8 +6,8 @@ while True:
     except EOFError:
         new_grocery_list = []
         [new_grocery_list.append(x) for x in grocery_list if x not in new_grocery_list]
-        sorted_list = new_grocery_list.sort()
-        [print(f"{grocery_list.count(x)} {x}") for x in sorted_list]
+        new_grocery_list.sort()
+        [print(f"{grocery_list.count(x)} {x}") for x in new_grocery_list]
         quit()
 
     grocery_list.append(item)
