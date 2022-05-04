5aac5f02bed0c2a6895d83d6abbbbbffbb849728
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-04 18:54:15 +0000

    Wed, May 4, 2022, 6:54 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -5,6 +5,7 @@ while True:
         item = input("Type item: ").upper()
     except EOFError:
         new_grocery_list = []
+        [new_grocery_list.append(x) for x in grocery_list if x not in new_grocery_list]
         quit()
 
     grocery_list.append(item)
