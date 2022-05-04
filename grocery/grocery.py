f1740078dbff31c235124f1bd814be9552944407
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-04 18:35:19 +0000

    Wed, May 4, 2022, 6:35 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,5 +1,9 @@
-try:
-    item = input("Type item: ").upper()
-except EOFError:
-    quit()
+grocery_list = []
 
+while True:
+    try:
+        item = input("Type item: ").upper()
+    except EOFError:
+        quit()
+
+    grocery_list.append(item)
