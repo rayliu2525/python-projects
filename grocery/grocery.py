fd3bb65551b1bee8063bbfe1f63d7a1d976d7b62
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-04 20:18:53 +0000

    Wed, May 4, 2022, 8:18 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -7,7 +7,8 @@ while True:
         new_grocery_list = []
         [new_grocery_list.append(x) for x in grocery_list if x not in new_grocery_list]
         new_grocery_list.sort()
-        [print(f"\n{grocery_list.count(x)} {x}") for x in new_grocery_list]
+        print()
+        [print(f"{grocery_list.count(x)} {x}") for x in new_grocery_list]
         quit()
 
     grocery_list.append(item)
