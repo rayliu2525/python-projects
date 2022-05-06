9c10df1cd4f1c9aae4c25fb912de7565404bb5e2
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-06 21:09:35 +0000

    Fri, May 6, 2022, 9:09 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,6 +17,9 @@ while True:
     try:
         date = input("Date: ")
         month, day, year = date.split("/")
+        month = int(month)
+        day = int(day)
+        year = int(year)
         if month in list(range(1,13)) and day in list(range(1,32)):
             break
     except:
@@ -25,9 +28,11 @@ while True:
             old_month, old_day, year = date.split(" ")
             month = month_list.index(old_month) + 1
             day = old_day.replace(",", "")
+            day = int(day)
+            year = int(year)
             if month in list(range(1,13)) and day in list(range(1,32)):
                 break
         except:
             pass
 
-print (f"{year}-{int(month):02}-{int(day):02}")
+print (f"{year}-{month:02}-{day:02}")
