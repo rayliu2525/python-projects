085650e6e13a12b667980b668bb18e5ad9ee02eb
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-06 18:59:00 +0000

    Fri, May 6, 2022, 6:59 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,5 +17,5 @@ while True:
     try:
         date = input("Date: ")
         month, day, year = date.split(/)
-        if month in list(range(1,13))
+        if month in list(range(1,13)) and day in list(range(1,32)) and year 
     except:
