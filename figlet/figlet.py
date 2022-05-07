5a44f558a5b708b3444aa91da35f1a1eac0ec0de
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-07 23:37:47 +0000

    Sat, May 7, 2022, 11:37 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,9 +6,6 @@ figlet = Figlet()
 
 font_list = figlet.getFonts()
 
-if ("-f" or "-font") not in sys.argv[1] or sys.argv[2] not in font_list:
-    sys.exit
-
 
 
 text = input("Input: ")
@@ -18,5 +15,9 @@ if len(sys.argv) == 1:
     print(figlet.renderText(text))
 
 if len(sys.argv) == 3:
+
+    if ("-f" or "-font") not in sys.argv[1] or sys.argv[2] not in font_list:
+        sys.exit
+
     figlet.setFont(random.choice(sys.argv[2]))
     print(figlet.renderText(text))
