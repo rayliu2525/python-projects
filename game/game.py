89bad417b46e279fcb44f932cce2a51235863828
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-11 14:06:10 +0000

    Wed, May 11, 2022, 2:06 PM +00:00

--- game/game.py
+++ game/game.py
@@ -5,6 +5,8 @@ while True:
         level = input("Level: ")
         level = int(level)
             if level > 0:
+                break
+    except:
 
 rand_int = random.randint(1, level)
 guess = input("Guess: ")
