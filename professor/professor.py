2b050b4467b534406e716306bed3c6c57672469d
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-12 00:54:11 +0000

    Thu, May 12, 2022, 12:54 AM +00:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,7 +2,7 @@ import random
 
 
 def main():
-    ...
+    level = get_level()
 
 
 def get_level():
@@ -16,8 +16,12 @@ def get_level():
             print("type valid number")
 
 def generate_integer(level):
-    if 
-
+    if level == 1:
+        return randint(1, 9)
+    if level == 2:
+        return randint(10, 99)
+    if level == 3:
+        return randint(100, 999)
 
 if __name__ == "__main__":
     main()
