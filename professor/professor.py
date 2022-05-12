0f30fd90ac024d9f18e55f361c78cc9728b9b138
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-12 00:32:48 +0000

    Thu, May 12, 2022, 12:32 AM +00:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,1 +1,18 @@
-level = input(")
+import random
+
+
+def main():
+    ...
+
+
+def get_level():
+    while True:
+        level = 
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
