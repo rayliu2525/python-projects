62655c1f30bf10b5ef852090fb049bf0a38bec58
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-12 22:20:23 +0000

    Thu, May 12, 2022, 10:20 PM +00:00

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-def merge_sort(arr):
-    if len(arr) > 1:
-        left_arr = arr[:len(arr)//2]
-        right_arr = arr[len(arr)//2:]
-
-
-print("hello") print("hello")
+x = print('{0:.16f}'.format(1.6))
+print(type(x))
