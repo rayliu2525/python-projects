9dcff86df233b18c1e27c2e3e6f3313c1e12ab9e
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-12 21:58:37 +0000

    Thu, May 12, 2022, 9:58 PM +00:00

---
+++ bitcoin.py
@@ -0,0 +1,10 @@
+import sys
+
+try:
+    float(sys.argv[1])
+except IndexError:
+    print("Missing command-line argument")
+    sys.exit
+except ValueError:
+    print("Command-line argument is not a number")
+    sys.exit
