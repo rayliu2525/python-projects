56ec6d28729a37492d0bee062118531c608cb9c5
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-12 22:08:26 +0000

    Thu, May 12, 2022, 10:08 PM +00:00

--- bitcoin.py
+++ bitcoin.py
@@ -2,10 +2,12 @@ import sys
 import requests
 
 try:
-    float(sys.argv[1])
+    bitcoin_number = float(sys.argv[1])
 except IndexError:
     print("Missing command-line argument")
     sys.exit
 except ValueError:
     print("Command-line argument is not a number")
     sys.exit
+
+r = requests.get('https://api.github.com/events')
