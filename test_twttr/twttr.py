57af9463ca3ac5c60c08d7535b527e8f2082db8c
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-13 15:56:54 +0000

    Fri, May 13, 2022, 3:56 PM +00:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -11,7 +11,12 @@ def main():
 
 
 def shorten(word):
-    ...
+    lst = []
+    for char in word:
+        if char not in ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]:
+            lst.append(char)
+    z = "".join(lst)
+    print(z)
 
 
 if __name__ == "__main__":
