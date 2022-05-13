3c4140b31403efdc29eeb05f5caf0e1f8cf61c68
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-13 04:08:49 +0000

    Fri, May 13, 2022, 4:08 AM +00:00

--- bitcoin/test.py
+++ bitcoin/test.py
@@ -3,5 +3,5 @@ import requests
 import pprint
 
 r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-r = json.dumps(r)
-print(r)
+r = r.json()
+bitcoin_usd = r["bpi"]["USD"]["rate_float"]
