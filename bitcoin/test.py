4f607824428570aecfd74e29ae0b48cd6e588b57
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-13 03:52:01 +0000

    Fri, May 13, 2022, 3:52 AM +00:00

--- bitcoin/test.py
+++ bitcoin/test.py
@@ -4,6 +4,4 @@ import requests
 r = requests.get('https://api.github.com/events')
 r = json.dumps(r.text)
 r = json.loads(r)
-bitcoin_usd = r["bpi"]["USD"]["rate_float"]
-
-print(bitcoin_usd)
+print(r)
