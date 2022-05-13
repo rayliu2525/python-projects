0cc2a9c6d32fcd57ae0f67e7078e9ca9fd64481c
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-13 19:38:19 +0000

    Fri, May 13, 2022, 7:38 PM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -3,7 +3,8 @@ import twttr
 def test_shorten1():
     twttr.shorten("My name is Raymond")
 
-
+def test_shorten2():
+    twttr.shorten("Hello, I love to eat beef")
 
 def test_shorten2():
     twttr.shorten("Hello, I love to eat beef")
