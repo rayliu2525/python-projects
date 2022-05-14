b7ac04c29bc8fa435ed03e3ee03562cb42dc4a54
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-14 16:24:10 +0000

    Sat, May 14, 2022, 4:24 PM +00:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -2,3 +2,9 @@ from bank import value
 
 def test_hello():
     assert value("hello") == 0
+
+def test_h():
+    assert value("honk") == 20
+
+def test_otherwise():
+    assert value("adsfdsf") == 100
