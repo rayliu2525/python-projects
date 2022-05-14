4c7bc429eee9c479ad3bc535cf0634570edca3cb
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-14 18:55:07 +0000

    Sat, May 14, 2022, 6:55 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -3,5 +3,9 @@ from plates import is_valid
 def test_two_letters():
     assert is_valid("JK") == True
 
-def test_num_middle():
-    assert is_valid("JK00JK") == True
+def test_num_end():
+    assert is_valid("JK00") == True
+
+def test_punctuations():
+    assert is_valid("JK00!") == False
+
