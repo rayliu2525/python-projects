a7df067fdf0f5594dde4048c9101f8b131817b37
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-14 18:53:41 +0000

    Sat, May 14, 2022, 6:53 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,4 +1,7 @@
 from plates import is_valid
 
 def test_two_letters():
-    assert is_valid("jk") == True
+    assert is_valid("JK") == True
+
+def test_num_middle():
+    assert is_valid("JK00JK") == True
