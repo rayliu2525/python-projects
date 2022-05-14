c18edc9404a52c8977386c402f786ef4cae33d45
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-14 16:08:21 +0000

    Sat, May 14, 2022, 4:08 PM +00:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -6,13 +6,13 @@ def main(greeting):
 def value(greeting):
     x = input("Type your greeting: ").lower().strip()
 
-if x[0:5] == "hello":
-    return 0
-elif x[0] == "h":
-    return 20
-else:
-    return 100
+    if x[0:5] == "hello":
+        return 0
+    elif x[0] == "h":
+        return 20
+    else:
+        return 100
 
 
 if __name__ == "__main__":
-    main()
+    main("hello")
