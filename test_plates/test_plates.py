0a0f8910eb7f658fbbcadae5992a9d62cecefb20
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-14 19:02:22 +0000

    Sat, May 14, 2022, 7:02 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,10 +1,10 @@
 from plates import is_valid
 
 def test_two_letters():
-    assert is_valid("AAA222") == True
+    assert is_valid("222") == False
 
 def test_num_end():
-    assert is_valid("JK50") == True
+    assert is_valid("JK500000") == False
 
 def test_punctuations():
     assert is_valid("JK00!") == False
