7f71879610f9ead92daadd41145e739ad2b97a83
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-16 16:01:30 +0000

    Mon, May 16, 2022, 4:01 PM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -3,7 +3,28 @@ def main():
 
 
 def convert(fraction):
-    ...
+    try:
+        x, y = fraction.split("/")
+
+    except ValueError:
+        print("try again")
+        continue
+
+    try:
+        if float(x).is_integer() == False or float(y).is_integer() == False:
+            continue
+    except ValueError:
+        print("try again")
+        continue
+
+    x = int(x)
+    y = int(y)
+
+    try:
+        gas_percent = f"{round(x / y * 100)}%"
+    except ZeroDivisionError:
+        print("try again")
+        continue
 
 
 def gauge(percentage):
