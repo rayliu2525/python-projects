35593581da373e2791681aab606e4053c6332f74
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-16 18:38:12 +0000

    Mon, May 16, 2022, 6:38 PM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -4,5 +4,5 @@ def test_convert_full():
     assert convert("3/4") == 75
 
 def test_convert_full():
-    with pytest.raises(ZeroDivisionError)
-        fraction = "3/0"
+    with pytest.raises(ZeroDivisionError) as info:
+        raise ZeroDivisionError("try again")
