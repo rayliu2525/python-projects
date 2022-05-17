43896da5dce38bf296cd03fd8e0d26efa7d01475
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-17 14:33:59 +0000

    Tue, May 17, 2022, 2:33 PM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -2,9 +2,3 @@ from fuel import convert, gauge
 
 def test_convert_normal():
     assert convert("3/4") == 75
-
-def test_gauge_full():
-    assert gauge("100") == "F"
-
-def test_gauge_empty():
-    assert gauge("0") == "E"
