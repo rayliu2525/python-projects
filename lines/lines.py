45fe826ea5b62923ada0aeed8ee1f56fa1e4a627
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-18 17:46:56 +0000

    Wed, May 18, 2022, 5:46 PM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -6,3 +6,11 @@ if sys.argv == 2 and sys.argv[1] == .py:
         sum_lines = sum(lines)
     print(sum_lines)
 
+if sys.argv < 2:
+    print("Too few command-line arguments")
+    sys.exit()
+
+elif sys.argv >2:
+    print("Too many command-line arguments")
+
+else 
