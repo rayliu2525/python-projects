5132af00c983863310179a0d2266eef1c9a99d5f
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-18 17:43:52 +0000

    Wed, May 18, 2022, 5:43 PM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,5 +1,8 @@
 import sys
 
-with open(sys.argv[1]) as file:
-    lines = [1 for line in file if (line.rstrip() and line[0] != \#)]
-    sum_lines = sum(lines)
+if sys.argv == 2 and sys.argv[1] == .py:
+    with open(sys.argv[1]) as file:
+        lines = [1 for line in file if (line.rstrip() and line[0] != \#)]
+        sum_lines = sum(lines)
+    print(sum_lines)
+
