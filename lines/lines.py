dff9833e0f9705a1f7ecddf65693d8ee51c638a6
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-18 20:58:54 +0000

    Wed, May 18, 2022, 8:58 PM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -2,18 +2,18 @@ import sys
 
 if sys.argv == 2 and sys.argv[1][:-3:-1] == ".py":
     with open(sys.argv[1]) as file:
-        lines = [1 for line in file if (line.rstrip() and line[0] != \#)]
+        lines = [1 for line in file if (line.rstrip() and line[0] != "#")]
         sum_lines = sum(lines)
     print(sum_lines)
 
-if sys.argv < 2:
+if len(sys.argv) < 2:
     print("Too few command-line arguments")
     sys.exit()
 
-elif sys.argv >2:
+elif len(sys.argv) > 2:
     print("Too many command-line arguments")
     sys.exit
 
-elif sys.argv[1] == False or sys.argv[1][:-3:-1] != ".py"
+elif sys.argv[1] == False or sys.argv[1][:-3:-1] != ".py":
     print("file does not exist or is the wrong type")
     sys.exit
