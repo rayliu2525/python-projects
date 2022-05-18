dd10c8c36c3665cf5dfa4e58a1aab3d7cfac1b0f
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-18 17:40:34 +0000

    Wed, May 18, 2022, 5:40 PM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,5 +1,5 @@
 import sys
 
 with open(sys.argv[1]) as file:
-    lines = [1 for line in file if (line.rstrip() and line[0] != #)]
+    lines = [1 for line in file if (line.rstrip() and line[0] != \#)]
     sum_lines = sum(lines)
