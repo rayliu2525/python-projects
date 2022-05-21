3705901ee4b502e519ca61495eea84e942366679
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-21 15:35:10 +0000

    Sat, May 21, 2022, 3:35 PM +00:00

--- pizza/test1.py
+++ pizza/test1.py
@@ -1,10 +1,15 @@
 import tabulate
 import csv
 
-with open("regular.csv", "r") as regular:
-    csv_reader = csv.reader(regular)
-    regular_list = []
-    for row in csv_reader:
-        regular_list.append(row)
+try:
+    with open("regular.csv", "r") as regular:
+        csv_reader = csv.reader(regular)
+        regular_list = []
+        for row in csv_reader:
+            regular_list.append(row)
+except:
+    print("wrong")
+finally:
+    print("w/e")
 
 print(regular_list)
