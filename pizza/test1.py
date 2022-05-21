20bf44fbc972ef9fd5548a840dba39b51d0ade92
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-21 15:20:33 +0000

    Sat, May 21, 2022, 3:20 PM +00:00

--- pizza/test1.py
+++ pizza/test1.py
@@ -5,6 +5,4 @@ with open("regular.csv", "r") as regular:
     csv_reader = csv.reader(regular)
     regular_list = []
     for row in csv_reader:
-        regular_list = regular_list.append(row)
-
-print(regular_list)
+        print(row)
