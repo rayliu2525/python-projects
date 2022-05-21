8ab28cc8daa7d12f9f568f775ac486017b254081
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-21 15:01:36 +0000

    Sat, May 21, 2022, 3:01 PM +00:00

--- pizza/test.py
+++ pizza/test.py
@@ -1,6 +1,10 @@
 import tabulate
 
 with open("regular.csv") as regular:
+    regular_list = []
+    for l in regular:
+        regular_list.append(l)
+
     print(tabulate(regular, headers=["Regular Pizzar", "Small", "Large"], tablefmt="grid"))
 
 
