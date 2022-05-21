1a7d0af901b72ae6e00da74343cd41f9e726739d
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-21 14:08:45 +0000

    Sat, May 21, 2022, 2:08 PM +00:00

--- pizza/test.py
+++ pizza/test.py
@@ -2,3 +2,7 @@ import tabulate
 
 with open("regular.csv") as regular:
     print(tabulate(regular, headers=["Regular Pizzar", "Small", "Large"], tablefmt="grid"))
+
+
+
+make list of lists from regular
