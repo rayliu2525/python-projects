e17eda1f4bb653183bc0b9fb540a4b6e9146a97c
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-23 22:05:34 +0000

    Mon, May 23, 2022, 10:05 PM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -6,7 +6,7 @@ with open(sys.argv[1]) as csv_file:
     next(csv_reader)
 
     with open(sys.argv[2], w) as csv_file:
-        csv_dictwriter = csv.DictWriter(sys.argv[2], fieldnames=["first", "last", "house"])
+        csv_dictwriter = csv.DictWriter(csv_file, fieldnames=["first", "last", "house"])
             for row in csv_reader:
                 first, last = split(row[0], ", ")
                 csv_dictwriter.writerow({
