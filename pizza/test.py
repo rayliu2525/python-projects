ab777c5528c4e395b6cb302b916c7ed804e9cb34
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-23 00:34:28 +0000

    Mon, May 23, 2022, 12:34 AM +00:00

--- pizza/test.py
+++ pizza/test.py
@@ -2,11 +2,12 @@ import sys
 import tabulate
 import csv
 
+regular_list = []
+
 
 with open("regular.csv", "r") as regular:
     csv_reader = csv.reader(regular)
+    for row in csv_reader:
+        regular_list.append(row)
 
-regular_list = []
-for row in csv_reader:
-    regular_list.append(row)
 print(tabulate(regular_list, headers=["Sicilian Pizza", "Small", "Large"], tablefmt="grid"))
