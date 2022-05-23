2d9a8990991b8209fd1ebb026c3dc4f868fbcb83
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-23 00:26:26 +0000

    Mon, May 23, 2022, 12:26 AM +00:00

--- pizza/test.py
+++ pizza/test.py
@@ -1,9 +1,6 @@
 import sys
 import tabulate
 
-try:
-    if (len(sys.argv) != 2 or sys.argv[1][-4:] != ".csv" or open(sys.argv[1])):
-        sys.exit
-    else:
-        pizza_file_object = open(sys.argv[1])
-        print(pizza_file_object)
+
+pizza_file_object = open(sys.argv[1])
+print(pizza_file_object)
