c2f3f254f16386870c27a47a6a065558fa1926b0
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-23 02:11:40 +0000

    Mon, May 23, 2022, 2:11 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -12,7 +12,7 @@ try:
             regular_list = []
             for row in csv_reader:
                 regular_list.append(row)
-            print(tabulate(regular_list, headers=["Sicilian Pizza", "Small", "Large"], tablefmt="grid"))
+            print(tabulate.tabulate(regular_list, headers=["Sicilian Pizza", "Small", "Large"], tablefmt="grid"))
 
 
 except Exception as e:
