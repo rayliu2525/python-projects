0a3306e40df1288e8b728f9e4617737caf49cd95
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-23 21:52:43 +0000

    Mon, May 23, 2022, 9:52 PM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,6 +5,8 @@ with open(sys.argv[1]) as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
 
-    with open(sys.argv[1], w) as 
+    with open(sys.argv[1], w) as csv_file:
+        csv_dictwriter = csv.D
+
     for row in csv_reader:
         first, last = split(row[0], ", ")
