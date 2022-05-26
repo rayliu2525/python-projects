536b8137d5fc5ed9fe8da43a3283b18664bd9b4a
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-26 18:13:01 +0000

    Thu, May 26, 2022, 6:13 PM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,3 +1,7 @@
 import sys
 
-if len(sys.argv) 
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
+elif sys.argv[1]
