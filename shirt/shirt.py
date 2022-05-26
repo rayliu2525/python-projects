dfdd9250e8900d0a493384a762fc03be9aa71b0f
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-26 19:46:55 +0000

    Thu, May 26, 2022, 7:46 PM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -12,3 +12,5 @@ elif os.splitext(lower(sys.argv[1]))[1] != os.splitext(lower(sys.argv[2]))[1]:
     sys.exit("Extensions are not the same")
 elif not os.path.exists(sys.argv[1]):
     sys.exit("Input file does not exist!")
+
+with image.open()
