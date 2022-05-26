e7e71eb156a4676ebee7aa04008148800ed5b543
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-26 22:02:20 +0000

    Thu, May 26, 2022, 10:02 PM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -6,9 +6,9 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-elif os.splitext(lower(sys.argv[1]))[1] not in (".jpg", ".jpeg", ".png") and os.splitext(lower(sys.argv[2]))[1] not in (".jpg", ".jpeg", ".png"):
+elif os.path.splitext(sys.argv[1])[1].lower() not in (".jpg", ".jpeg", ".png") and os.path.splitext(sys.argv[2])[1].lower() not in (".jpg", ".jpeg", ".png"):
     sys.exit("Incorrect extensions")
-elif os.splitext(lower(sys.argv[1]))[1] != os.splitext(lower(sys.argv[2]))[1]:
+elif os.path.splitext(sys.argv[1])[1].lower() != os.path.splitext(sys.argv[2])[1].lower():
     sys.exit("Extensions are not the same")
 elif not os.path.exists(sys.argv[1]):
     sys.exit("Input file does not exist!")
