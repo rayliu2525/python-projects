e9b3436e35756032e33363d61a634e76536f52f1
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-27 17:14:05 +0000

    Fri, May 27, 2022, 5:14 PM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -15,7 +15,7 @@ elif not os.path.exists(sys.argv[1]):
     sys.exit("Input file does not exist!")
 
 with Image.open(sys.argv[1]) as input_image:
-    with Image.open("shirt.png", mode="w") as shirt_image:
+    with Image.open("shirt.png") as shirt_image:
         input_image = ImageOps.fit(input_image, shirt_image.size)
         input_image.paste(shirt_image)
         input_image.save(sys.argv[2])
