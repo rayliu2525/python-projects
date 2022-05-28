ce0921ccc9eb2fbda3cefd4752b710910607de30
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-28 17:47:18 +0000

    Sat, May 28, 2022, 5:47 PM +00:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -4,5 +4,5 @@ from numb3rs import validate
 def test_valid():
     assert validate("1.1.1.1") == True
 
-def test_valid():
-    assert validate("1.1.1.1") == True
+def test_invalid():
+    assert validate("1.1.1.") == False
