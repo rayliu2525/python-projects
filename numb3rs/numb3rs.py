359716b2cc6417f42c1a13052e733742e0f0be23
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-28 17:37:15 +0000

    Sat, May 28, 2022, 5:37 PM +00:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,8 +7,7 @@ def main():
 
 
 def validate(ip):
-    ip_address = input("IPv4 Address: ")
-    if re.search("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ip_address):
+    if re.search("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ip):
         return True
     else:
         return False
