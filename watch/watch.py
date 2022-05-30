c8e7abab87bcf9cd686dfa73feb22f8762c2a67e
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-30 20:39:57 +0000

    Mon, May 30, 2022, 8:39 PM +00:00

--- watch/watch.py
+++ watch/watch.py
@@ -7,14 +7,16 @@ def main():
 
 
 def parse(s):
-    
 
-    match = re.search("src=", s)
-    URL_start_index = match.end() + 1
-    URL_end_index = s.find("\"", URL_start_index)
-    URL = s[URL_start_index:URL_end_index]
-    short_URL = URL.replace("be.com/embed", ".be")
-    return short_URL
+    if re.search("youtube.com", s):
+        match = re.search("src=", s)
+        URL_start_index = match.end() + 1
+        URL_end_index = s.find("\"", URL_start_index)
+        URL = s[URL_start_index:URL_end_index]
+        short_URL = URL.replace("be.com/embed", ".be")
+        return short_URL
+    else:
+        return None
 
 
 ...
