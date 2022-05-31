859c18153c8c0a51844f4571936914f8f4a0479a
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-31 16:33:08 +0000

    Tue, May 31, 2022, 4:33 PM +00:00

--- working/working.py
+++ working/working.py
@@ -11,11 +11,12 @@ def convert(s):
         beg_time, end_time = s.split(" to ")
         if re.search("^([1-9]|1[0-2]): (A|P)M)$", beg_time) and re.search("^([1-9]|1[0-2]): (A|P)M)$", end_time):
             l_beg_time, r_beg_time = beg_time.split(" ")
-            beg_time = l_beg_time + ":00" + r_beg_time
+            beg_time = l_beg_time + ":00 " + r_beg_time
             l_end_time, r_end_time = end_time.split(" ")
-            end_time = l_end_time + ":00" + r_end_time"
-            
-        if beg_time.split(":")[0]
+            end_time = l_end_time + ":00 " + r_end_time"
+
+        if PM in beg_time
+            time_24 = 
 
 
     else:
