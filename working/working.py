049a3077ac88b006eb5cd0fcb6615102ac20927b
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-05-31 16:31:09 +0000

    Tue, May 31, 2022, 4:31 PM +00:00

--- working/working.py
+++ working/working.py
@@ -10,8 +10,11 @@ def convert(s):
     if re.search("^([1-9]|1[0-2])|([0-9]|1[0-2]):[0-5][0-9]) (A|P:M) to ([1-9]|1[0-2])|([0-9]|1[0-2]):[0-5][0-9]) (A|P)M$", s):
         beg_time, end_time = s.split(" to ")
         if re.search("^([1-9]|1[0-2]): (A|P)M)$", beg_time) and re.search("^([1-9]|1[0-2]): (A|P)M)$", end_time):
-            beg_time.split()
-            end_time = f"{end_time}:00"
+            l_beg_time, r_beg_time = beg_time.split(" ")
+            beg_time = l_beg_time + ":00" + r_beg_time
+            l_end_time, r_end_time = end_time.split(" ")
+            end_time = l_end_time + ":00" + r_end_time"
+            
         if beg_time.split(":")[0]
 
 
