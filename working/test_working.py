dfbffc803583bed0507a39803e87cbcbf17cfe45
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-01 04:37:58 +0000

    Wed, Jun 1, 2022, 4:37 AM +00:00

--- working/test_working.py
+++ working/test_working.py
@@ -8,3 +8,6 @@ def test_nozero_convert():
 
 def test_both24_convert():
     assert convert("9:00 PM to 11:00 PM") == "21:00 to 23:00"
+
+def test_both24_convert():
+    assert convert("9:00 PM - 11:00 PM") == "21:00 to 23:00"
