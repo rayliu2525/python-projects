e2437093781f6f5df510cfddfde18f2603de0870
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-01 03:34:48 +0000

    Wed, Jun 1, 2022, 3:34 AM +00:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,5 +1,5 @@
 import re
 import sys
 
-def test_00_convert:
-    assert convert("5:00 AM to 9:00 PM") = "5:00 to 21:00"
+def test_00_convert():
+    assert convert("5:00 AM to 9:00 PM") == "5:00 to 21:00"
