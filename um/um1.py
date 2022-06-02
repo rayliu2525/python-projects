27a54e60c8ec774d7c9eba18f698f532666a5ab9
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-02 19:34:33 +0000

    Thu, Jun 2, 2022, 7:34 PM +00:00

--- um/um1.py
+++ um/um1.py
@@ -9,8 +9,11 @@ def main():
 def count(s):
 
     if re.
-    
+
     matches = re.finditer("[^a-zA-Z]ums[^a-zA-Z]", s, re.IGNORECASE)
+    matches1 = re.finditer("ums[^a-zA-Z]", s, re.IGNORECASE)
+
+
     matches = tuple(matches)
     return len(matches)
 
