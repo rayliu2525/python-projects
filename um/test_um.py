03fd19cc8afa79f941797dfe91176d3bac72ed51
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-02 19:11:42 +0000

    Thu, Jun 2, 2022, 7:11 PM +00:00

--- um/test_um.py
+++ um/test_um.py
@@ -3,5 +3,8 @@ from um import count
 def test_separate_um():
     assert count("hello, um, um, hello") == 2
 
-def test_separate_um():
-    assert count("hello, um, um, hello") == 2
+def test_substring_um():
+    assert count("hello, hum, um, hello") == 1
+
+def test_isolated_um():
+    assert count("hello, hum um um hello") == 2
