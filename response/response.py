5d8c2a8d47d9e98bdecfe0006de06223c30db0eb
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-03 04:18:13 +0000

    Fri, Jun 3, 2022, 4:18 AM +00:00

--- response/response.py
+++ response/response.py
@@ -4,10 +4,13 @@ def main():
     print(validate(input("Type Email: ")))
 
 def validate(s):
-    if validators.email(s, allow_empty = True):
-        return "Valid"
-    else:
-        return "Invalid"
+    try:
+        if validators.email(s, allow_empty = True):
+            return "Valid"
+        else:
+            return "Invalid"
+    except:
+        print("Invalid")
 
 if __name__ == "__main__":
     main()
