9c278bd48eec717a9c48c5af1991a383c23b5c47
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-03 04:14:40 +0000

    Fri, Jun 3, 2022, 4:14 AM +00:00

--- response/response.py
+++ response/response.py
@@ -4,7 +4,7 @@ def main():
     print(validate(input("Type Email: ")))
 
 def validate(s):
-    if validate.email(s):
+    if validators.email(s):
         return "Valid"
     else:
         return "Invalid"
