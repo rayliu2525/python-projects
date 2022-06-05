a3c9bb3a704423fd7d59ffcfd55f06d8723aadb0
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-05 19:49:54 +0000

    Sun, Jun 5, 2022, 7:49 PM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,13 +4,14 @@ import sys
 
 
 def main():
-    dob = input("Date of Birth: ")
-    if re.search("^[1-9]{4}-[1-9]{2}-[1-9]{2}$", dob) == None:
+    dob_input = input("Date of Birth: ")
+    if re.search("^[1-9]{4}-[1-9]{2}-[1-9]{2}$", dob_input) == None:
         sys.exit("Invaid date")
 
-    
-...
+    year, month, day = dob_input.split("-")
 
+    dob = date(year, month, day)
+    current_date = date.today()
 
 if __name__ == "__main__":
     main()
