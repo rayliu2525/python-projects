689d165ee9e5a5275a672da7ffc6c49524940346
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-05 19:43:58 +0000

    Sun, Jun 5, 2022, 7:43 PM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,11 +1,12 @@
 from datetime import date
 import re
+import sys
 
 
 def main():
     dob = input("Date of Birth: ")
     if re.search("^[1-9]{4}-[1-9]{2}-[1-9]{2}$", dob)
-
+        sys.exit("Invaid date")
 ...
 
 
