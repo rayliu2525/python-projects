cb7111a7165aa48576142419f5e6141bbe79b27e
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-05 21:26:54 +0000

    Sun, Jun 5, 2022, 9:26 PM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -10,12 +10,15 @@ def main():
         sys.exit("Invaid date")
 
     year, month, day = dob_input.split("-")
+    year = int(year)
+    month = int(month)
+    day = int(day)
 
     dob = date(year, month, day)
     current_date = date.today()
 
     delta = current_date - dob
-    minutes = round(delta.total_second() * 60)
+    minutes = round(delta.total_seconds() * 60)
 
     engine_object = inflect.engine()
     minutes_words = engine_object.number_to_words(minutes)
