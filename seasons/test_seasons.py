e2453a8ce29201ecec7861562d5291045566473d
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-06 03:27:50 +0000

    Mon, Jun 6, 2022, 3:27 AM +00:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,7 +1,9 @@
 from seasons import life_minutes
+import pytest
 
 def test_normal_life_minutes():
     assert life_minutes("1992-02-25") == "Fifteen million, nine hundred twenty-four thousand, nine hundred sixty minutes"
 
-def test_invalid_life_minutes():
-    assert life_minutes("1992-January-25") == "Invalid date"
+def test_error():
+    with pytest.raises(SystemExit):
+        life_minutes("1992-January-25")
