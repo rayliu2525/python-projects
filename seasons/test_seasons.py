7086f7d09da0dd705ffa54657d86eaeb9295343f
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-06 03:14:58 +0000

    Mon, Jun 6, 2022, 3:14 AM +00:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,7 +1,7 @@
 from seasons import life_minutes
 
-def normal_life_minutes():
+def test_normal_life_minutes():
     assert life_minutes("1992-02-25") == "Fifteen million, nine hundred twenty-four thousand, nine hundred sixty minutes"
 
-def invalid_life_minutes():
+def test_invalid_life_minutes():
     assert life_minutes("1992-January-25") == "Invalid date"
