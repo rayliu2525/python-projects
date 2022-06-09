89a46891ee24e2dd2ae0d75fd8e4ec0af4cfb233
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-09 02:54:15 +0000

    Thu, Jun 9, 2022, 2:54 AM +00:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,7 +1,7 @@
 class Jar:
     def __init__(self, capacity=12):
-        self.capacity = capacity
-        if is_integer(self.capacity) == False or self.capacity < 0:
+        self._capacity = capacity
+        if is_integer(self._capacity) == False or self._capacity < 0:
             raise ValueErrors
         self.cookies = 0
 
@@ -10,19 +10,18 @@ class Jar:
 
     def deposit(self, n):
         self.cookies += n
-        if n > self.capacity:
+        if n > self._capacity:
             raise ValueError
 
     def withdraw(self, n):
-        self.cookies -+ n
+        self.cookies -= n
         if n < 0:
             raise ValueError
 
     @property
     def capacity(self):
-        return self.capacity
+        return self._capacity
 
     @property
     def size(self):
         ...
-    
