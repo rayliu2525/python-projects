6c928383b9658ea16610719d911dadf3db187d54
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-17 18:24:33 +0000

    Fri, Jun 17, 2022, 6:24 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,5 +4,6 @@ name = input("Name: ")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.add_page()
-pdf.cell(txt="CS50 Shirtificate", align="C")
-pdf.image("shirtificate.png", )
+#pdf.cell(txt="CS50 Shirtificate", align="C")
+pdf.image("shirtificate.png", x=0, y=0)
+pdf.output("hello.pdf")
