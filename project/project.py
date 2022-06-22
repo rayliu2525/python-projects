201abda3656fa892384db85a7a32dfc41253d066
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-22 21:56:56 +0000

    Wed, Jun 22, 2022, 9:56 PM +00:00

--- project/project.py
+++ project/project.py
@@ -2,3 +2,6 @@ def main():
     starting_balance = input("How much money is in your bank account? ")
     interest_rate = input("What interest rate do you expect? ")
     length = input("How many years will your account accrue the aforementioned interest rate? ")
+
+def convert(x):
+    return int(x)
