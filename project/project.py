ac5f7e0bdb18ec40ac1fe89bb0523f567a3b5c44
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-22 21:59:20 +0000

    Wed, Jun 22, 2022, 9:59 PM +00:00

--- project/project.py
+++ project/project.py
@@ -1,5 +1,12 @@
+import sys
+
 def main():
-    starting_balance = input("How much money is in your bank account? ")
+    try:
+        starting_balance = input("How much money is in your bank account? ")
+        starting_balance = convert(starting_balance)
+    except:
+        sys.exit("Not a valid number")
+
     interest_rate = input("What interest rate do you expect? ")
     length = input("How many years will your account accrue the aforementioned interest rate? ")
 
