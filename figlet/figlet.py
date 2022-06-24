2a2db6ffe4006a5af9d48533e4c149e02a52f528
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-24 02:17:00 +0000

    Fri, Jun 24, 2022, 2:17 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -10,7 +10,7 @@ if len(sys.argv) != 1:
     if len(sys.argv) != 3:
         sys.exit("1")
 
-if ("-f" or "--font") not in sys.argv[1] or sys.argv[2] not in font_list:
+if ("-f" or "--font") not sys.argv[1] or sys.argv[2] not in font_list:
     sys.exit("1")
 
 text = input("Input: ")
