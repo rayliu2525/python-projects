0d11733804fb8b026beaf88d63e532b3afb3aa63
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-24 01:32:36 +0000

    Fri, Jun 24, 2022, 1:32 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -10,6 +10,12 @@ font_list = figlet.getFonts()
 
 text = input("Input: ")
 
+if len(sys.argv) not 1:
+    sys.exit()
+
+if len(sys.argv) not 3:
+    sys.exit()
+
 if len(sys.argv) == 1:
     figlet.setFont(font=(random.choice(font_list)))
     print(figlet.renderText(text))
